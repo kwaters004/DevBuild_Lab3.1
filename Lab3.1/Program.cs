27448585490532b02df84935d63187310cb73e9a
Kirk Waters <kirkwaters@quickenloans.com>
2021-03-22 14:32:06 -0400

    Updates

---
+++ Lab3.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WElcome to Lab 3.1!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
